0cdc47958f8576c37777d05bd317043d600eb5e4
Samuel Berthe <dev@samuel-berthe.fr>
2023-12-06 02:20:58 +0100

    doc

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -5,6 +5,7 @@ import styles from './styles.module.css';
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  src: string;
   description: JSX.Element;
 };
 
@@ -12,6 +13,8 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Separation of concerns',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    // src: "https://www.svgrepo.com/download/520485/research.svg",
+    src: "https://www.svgrepo.com/download/514345/street-sign.svg",
     description: (
       <>
         DO was designed from the ground up to increase modularity
@@ -22,6 +25,8 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Application lifecycle',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    // src: "https://www.svgrepo.com/download/520498/clock.svg",
+    src: "https://www.svgrepo.com/download/514347/compass.svg",
     description: (
       <>
         DO provides an API to check health of the active services and to
@@ -32,6 +37,8 @@ const FeatureList: FeatureItem[] = [
   {
     title: 'Easy debugging',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    // src: "https://www.svgrepo.com/download/520483/quiz.svg",
+    src: "https://www.svgrepo.com/download/514336/telescope.svg",
     description: (
       <>
         Debugging IoC can be painful.
@@ -42,11 +49,12 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg,src, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={src} className={styles.featureSvg} />
+        {/* <Svg src="https://www.svgrepo.com/download/520496/board.svg" className={styles.featureSvg} role="img" /> */}
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
