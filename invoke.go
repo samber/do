81b3d8f13197375cfb9d88b75d0fb9bdb8870b90
Samuel Berthe <dev@samuel-berthe.fr>
2023-12-30 19:39:13 +0100

    feat: auto-magically inject dependencies into struct

--- invoke.go
+++ invoke.go
@@ -2,9 +2,54 @@ package do
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
 	"strings"
+	"unsafe"
+
+	typetostring "github.com/samber/go-type-to-string"
 )
+
+// invokeAnyByName looks for a service by its tag.
+func invokeAnyByName(i Injector, name string) (any, error) {
+	var invokerChain []string
+
+	injector := getInjectorOrDefault(i)
+
+	vScope, isVirtualScope := injector.(*virtualScope)
+	if isVirtualScope {
+		invokerChain = vScope.invokerChain
+
+		if err := vScope.detectCircularDependency(name); err != nil {
+			return nil, err
+		}
+	}
+
+	serviceAny, serviceScope, found := injector.serviceGetRec(name)
+	if !found {
+		return nil, serviceNotFound(injector, name)
+	}
+
+	if isVirtualScope {
+		vScope.addDependency(injector, name, serviceScope)
+	}
+
+	service, ok := serviceAny.(ServiceAny)
+	if !ok {
+		return nil, serviceNotFound(injector, name)
+	}
+
+	instance, err := service.getInstanceAny(&virtualScope{invokerChain: append(invokerChain, name), self: serviceScope})
+	if err != nil {
+		return nil, err
+	}
+
+	serviceScope.onServiceInvoke(name)
+
+	injector.RootScope().opts.Logf("DI: service %s invoked", name)
+
+	return instance, nil
+}
 
 // invokeByName looks for a service by its name.
 func invokeByName[T any](i Injector, name string) (T, error) {
@@ -113,6 +158,60 @@ func invokeByGenericType[T any](i Injector) (T, error) {
 	injector.RootScope().opts.Logf("DI: service %s invoked", serviceAliasName)
 
 	return instance.(T), nil
+}
+
+// invokeByTag looks for a service by its tag.
+func invokeByTags(i Injector, structValue reflect.Value) error {
+	injector := getInjectorOrDefault(i)
+
+	// Ensure that servicePtr is a pointer to a struct
+	if structValue.Kind() != reflect.Ptr || structValue.Elem().Kind() != reflect.Struct {
+		return fmt.Errorf("DI: not a pointer")
+	}
+
+	structValue = structValue.Elem()
+
+	// Iterate through the fields of the struct
+	for i := 0; i < structValue.NumField(); i++ {
+		field := structValue.Type().Field(i)
+		fieldValue := structValue.Field(i)
+
+		serviceName, ok := field.Tag.Lookup(coalesce(injector.RootScope().opts.StructTagKey, DefaultStructTagKey))
+		if !ok {
+			continue
+		}
+
+		if !fieldValue.CanAddr() {
+			return fmt.Errorf("DI: Field is not addressable %s", field.Name)
+		}
+
+		if !fieldValue.CanSet() {
+			// When a field is not exported, we override it.
+			// See https://stackoverflow.com/questions/42664837/how-to-access-unexported-struct-fields/43918797#43918797
+			fieldValue = reflect.NewAt(fieldValue.Type(), unsafe.Pointer(fieldValue.UnsafeAddr())).Elem()
+		}
+
+		if serviceName == "" {
+			serviceName = typetostring.GetReflectValueType(fieldValue)
+		}
+
+		dependency, err := invokeAnyByName(injector, serviceName)
+		if err != nil {
+			return err
+		}
+
+		dependencyValue := reflect.ValueOf(dependency)
+
+		// Should be check before invocation, because we just built something that is not assignable to the field.
+		if !fieldValue.Type().AssignableTo(dependencyValue.Type()) {
+			return fmt.Errorf("DI: field '%s' is not assignable to service %s", field.Name, serviceName)
+		}
+
+		// Should not panic, since we checked CanAddr() and CanSet() earlier.
+		fieldValue.Set(dependencyValue)
+	}
+
+	return nil
 }
 
 // serviceNotFound returns an error indicating that the specified service was not found.
