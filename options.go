1d3425fe854969730306a707074e8a3efb311d74
Samuel Berthe <dev@samuel-berthe.fr>
2024-05-09 23:36:11 +0200

    feat: add support for multple hook handlers

--- options.go
+++ options.go
@@ -5,8 +5,12 @@ import "time"
 const DefaultStructTagKey = "do"
 
 type InjectorOpts struct {
-	HookAfterRegistration func(scope *Scope, serviceName string)
-	HookAfterShutdown     func(scope *Scope, serviceName string)
+	HookBeforeRegistration []func(scope *Scope, serviceName string)
+	HookAfterRegistration  []func(scope *Scope, serviceName string)
+	HookBeforeInvocation   []func(scope *Scope, serviceName string)
+	HookAfterInvocation    []func(scope *Scope, serviceName string, err error)
+	HookBeforeShutdown     []func(scope *Scope, serviceName string)
+	HookAfterShutdown      []func(scope *Scope, serviceName string, err error)
 
 	Logf func(format string, args ...any)
 
@@ -15,4 +19,40 @@ type InjectorOpts struct {
 	HealthCheckTimeout       time.Duration // default: no timeout
 
 	StructTagKey string
+}
+
+func (o *InjectorOpts) onBeforeRegistration(scope *Scope, serviceName string) {
+	for _, fn := range o.HookBeforeRegistration {
+		fn(scope, serviceName)
+	}
+}
+
+func (o *InjectorOpts) onAfterRegistration(scope *Scope, serviceName string) {
+	for _, fn := range o.HookAfterRegistration {
+		fn(scope, serviceName)
+	}
+}
+
+func (o *InjectorOpts) onBeforeInvocation(scope *Scope, serviceName string) {
+	for _, fn := range o.HookBeforeInvocation {
+		fn(scope, serviceName)
+	}
+}
+
+func (o *InjectorOpts) onAfterInvocation(scope *Scope, serviceName string, err error) {
+	for _, fn := range o.HookAfterInvocation {
+		fn(scope, serviceName, err)
+	}
+}
+
+func (o *InjectorOpts) onBeforeShutdown(scope *Scope, serviceName string) {
+	for _, fn := range o.HookBeforeShutdown {
+		fn(scope, serviceName)
+	}
+}
+
+func (o *InjectorOpts) onAfterShutdown(scope *Scope, serviceName string, err error) {
+	for _, fn := range o.HookAfterShutdown {
+		fn(scope, serviceName, err)
+	}
 }
