34c6dada46831a0dad1fcd66d72cba6ae392bf83
dhernando <david.hernando91@gmail.com>
2025-07-13 23:27:11 +0200

    use reflect instead of fmt.Sprintf to avoid comparing against a string

--- service.go
+++ service.go
@@ -2,6 +2,7 @@ package do
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type Service[T any] interface {
@@ -21,6 +22,11 @@ type shutdownableService interface {
 }
 
 func generateServiceName[T any]() string {
+	return generateServiceNameWithReflect[T]()
+}
+
+//nolint:unused
+func generateServiceNameWithSprintf[T any]() string {
 	var t T
 
 	// struct
@@ -29,9 +35,44 @@ func generateServiceName[T any]() string {
 		return name
 	}
 
-	// interface
+	//interface
 	return fmt.Sprintf("%T", new(T))
 }
+
+
+func generateServiceNameWithReflect[T any]() string {
+	var t T
+	// For non-pointer types, reflect.TypeOf(t) will never be nil.
+	// For pointer types, reflect.TypeOf(t) can be nil if t is nil.
+	typ := reflect.TypeOf(t)
+	if typ == nil {
+		return ""
+	}
+
+	if name := typ.String(); name != "" {
+		return name
+	}
+
+	return reflect.TypeOf(new(T)).String()
+}
+
+// func generateServiceName[T any]() string {
+// 	var t T
+// 	// For non-pointer types, reflect.TypeOf(t) will never be nil.
+// 	// For pointer types, reflect.TypeOf(t) can be nil if t is nil.
+// 	typ := reflect.TypeOf(t)
+// 	if typ == nil {
+// 		return ""
+// 	}
+//
+// 	name := typ.Name()
+// 	if name != "" {
+// 		return name
+// 	}
+//
+// 	return reflect.TypeOf(new(T)).Name()
+// }
+
 
 type Healthcheckable interface {
 	HealthCheck() error
