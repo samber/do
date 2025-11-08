c30e7db1df9a0be897e547dee54b363a56739141
Samuel Berthe <dev@samuel-berthe.fr>
2025-11-08 21:50:55 +0100

    chore: some CI/docs/tests improvements

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package do
 
 import (
-	"os"
+	"fmt"
 	"reflect"
+	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -25,14 +27,34 @@ func testWithTimeout(t *testing.T, timeout time.Duration) {
 	t.Helper()
 
 	testFinished := make(chan struct{})
-	t.Cleanup(func() { close(testFinished) })
+
+	t.Cleanup(func() {
+		close(testFinished)
+	})
+
+	line := ""
+	funcName := ""
+
+	var pc [1]uintptr
+	n := runtime.Callers(2, pc[:])
+	if n > 0 {
+		frames := runtime.CallersFrames(pc[:])
+		frame, _ := frames.Next()
+		line = frame.File + ":" + strconv.Itoa(frame.Line)
+		funcName = frame.Function
+	}
 
 	go func() {
 		select {
 		case <-testFinished:
 		case <-time.After(timeout):
-			t.Errorf("test timed out after %s", timeout)
-			os.Exit(1)
+			if line == "" || funcName == "" {
+				panic(fmt.Sprintf("Test timed out after: %v", timeout))
+			}
+			panic(fmt.Sprintf("%s: Test timed out after: %v\n%s", funcName, timeout, line))
+
+			// t.Errorf("Test timed out after: %v", timeout)
+			// os.Exit(1)
 		}
 	}()
 }
